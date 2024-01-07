95d36f266c7dc08624b614fbd0179a8f596832e4
Subham2942 <subham0464@gmail.com>
2024-01-07 23:38:24 +0530

    first commit for day1 and day2

--- Days/day2/tipCalculator.py
+++ Days/day2/tipCalculator.py
@@ -1,2 +1,16 @@
 print("Welcome to tip calculator")
-amount = int("What is the amount ")
+amount = int(input("What is the amount?\n "))
+valid = False
+
+people = 1
+tip = 0
+while not valid:
+    tip = int(input("What will be the Tip percentage?(eg. 10, 12 or 15)\n"))
+    people = int(input("How many people to split the bill?\n "))
+    if people > 0 and (tip >= 0):
+        valid = True
+    else:
+        print("Tip or number of people cannot be zero or less")
+   
+Result = (amount + amount*(tip/100)) / people
+print(f"The Amount to be paid by each person is Rs {Result}")
