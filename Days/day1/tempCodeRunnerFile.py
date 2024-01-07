eaa358720b78157a00906dd706a01b98f551f0eb
Subham2942 <subham0464@gmail.com>
2024-01-07 23:38:14 +0530

    first commit for day1 and day2

---
+++ Days/day1/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+cityName = str(input("What is the name of the City you grew up in?\n"))
+petName = str(input("What is the name of your pet?\n"))
+print(f"Your Band name could be {cityName} {petName}")
---
+++ Days/day2/tipCalculator.py
@@ -0,0 +1,2 @@
+print("Welcome to tip calculator")
+amount = int("What is the amount ")
